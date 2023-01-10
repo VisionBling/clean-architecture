705abaf12cd2445526b5b8fd2ef33e4a513e6f2d
Uloma Okenyi <cyndibilo@gmail.com>
2023-01-10 05:36:54 +0000

    mofdified patient controller

--- CleanArchitecture/CleanArchitecture.WebAPI/Controllers/DoctorsController.cs
+++ CleanArchitecture/CleanArchitecture.WebAPI/Controllers/DoctorsController.cs
@@ -16,7 +16,11 @@ namespace CleanArchitecture.WebAPI.Controllers
             _doctorService = doctorService;
         }
 
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="doctorDto"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<ActionResult<DoctorDto>> AddDoctor([FromBody] DoctorCreationDto doctorDto)
         {
