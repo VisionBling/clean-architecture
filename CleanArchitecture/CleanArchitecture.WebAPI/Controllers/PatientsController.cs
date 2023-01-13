0bb29d30d0ec098a829d3f07ed6c4f8112d66cfb
Uloma Okenyi <cyndibilo@gmail.com>
2023-01-13 20:39:32 +0000

    mofdified patient controller

--- CleanArchitecture/CleanArchitecture.WebAPI/Controllers/PatientsController.cs
+++ CleanArchitecture/CleanArchitecture.WebAPI/Controllers/PatientsController.cs
@@ -20,7 +20,7 @@ namespace CleanArchitecture.WebAPI.Controllers
        
 
         /// <summary>
-        ///add 
+        /// 
         /// </summary>
         /// <param name="patientDto"></param>
         /// <returns></returns>
