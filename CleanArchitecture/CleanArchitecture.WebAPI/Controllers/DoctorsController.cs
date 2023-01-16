ba794af4f41ad93cb3a73c6983131667acd9b2eb
Uloma Okenyi <cyndibilo@gmail.com>
2023-01-16 20:43:26 +0000

    refactor controller code

--- CleanArchitecture/CleanArchitecture.WebAPI/Controllers/DoctorsController.cs
+++ CleanArchitecture/CleanArchitecture.WebAPI/Controllers/DoctorsController.cs
@@ -29,7 +29,7 @@ namespace CleanArchitecture.WebAPI.Controllers
         }
 
       /// <summary>
-      /// 
+      /// get all doctors
       /// </summary>
       /// <returns></returns>
         [HttpGet]
