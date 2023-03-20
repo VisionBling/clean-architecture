a606f5558065bc5521c1deed4524c86ec6df1b09
Uloma Okenyi <cyndibilo@gmail.com>
2023-03-20 05:04:17 +0000

    swaggwer API

--- CleanArchitecture/CleanArchitecture.WebAPI/Program.cs
+++ CleanArchitecture/CleanArchitecture.WebAPI/Program.cs
@@ -21,7 +21,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Clean Architecture API V1"));
 }
 
 app.UseHttpsRedirection();
