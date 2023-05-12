7ad0960e4364d9551de42af2ead13ffaa06cc6b7
Uloma Okenyi <cyndibilo@gmail.com>
2023-05-12 02:47:18 +0000

    added Data transfer objects for the entities

---
+++ CleanArchitecture/CleanArchitecture.Application/DTOs/DoctorDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.DTOs
+{
+    public class DoctorDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Application/DTOs/PatientDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.DTOs
+{
+    public class PatientDto: PatientCreationDto
+    {
+        public Guid Id { get; set; }
+    }
+    public class PatientCreationDto
+    {
+        public string Name { get; set; }
+    }
+
+}
